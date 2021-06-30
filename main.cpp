16ba4098dc887ae6129a60fa7545cbd4f8a159c1
LuFipe <luizfelipecastrocoelho@gmail.com>
2021-06-30 16:42:16 -0300

    Modularização 2.2
    -Todas as subrotinas fortam adaptadas para a estrutura
    -Codigo ficou mais limpo e facil de compreender
    -Correção no metodo do calculo de precisao

--- main.cpp
+++ main.cpp
@@ -28,7 +28,8 @@ struct TabOD{
 	float tabVPProp[2][dim]={0};
 	float tabFrat[2][dim]={0};
 
-	int prec=5;
+	int prec_I=5;
+	float prec_O=0;
 	float totViagPre=0, totViagFut=0;
 };
 
@@ -37,139 +38,46 @@ void MostMatrix(float*, int, int);
 void MostMat2D(float*, int);
 void InitTab(TabOD*);
 void ShowTab(TabOD*);
+float CalcPreci(float*, int);
 
 //Declaracao das subrotinas dos metodos
-void MetUni(float*, float*, float*, float*, float, float, int);
-void MetMedio(float*, float*, float*, int, int);
-void MetDet(float*, float*, float*, float, float, int, int);
-void MetFrat(float*, float*, float*, float, float, int d, int p);
+void MetUni(float*, float*, float*, float*, float, float, int, float*);
+void MetMedio(float*, float*, float*, float*, int, int, float*);
+void MetDet(float*, float*, float*, float*, float, float, int, int, float*);
+void MetFrat(float*, float*, float*, float*, float*, float*, float, float, int, int, float*);
 
 //Variavel Global - Controlador de loop
 int i=0, j=0;
 float aux[2]={0};
 
 int main(){
-	//Dimençao Matriz
-	int dim=4;
-	//Declaracao e inicializacao de matrizes
-	float 
-	//Matrizes Base (hardcoding)
-		matrizOriginal[dim][dim] = {
-			{0.0,10.0,15.0,12.0},
-			{13.0,0.0,23.0,14.0},
-			{31.0,43.0,0.0,11.0},
-			{19.0,32.0,18.0,0}
-		},
-		
-		//Matriz dos Totais
-		matTot[2][dim]={0},
-		//Matriz Proporcao
-		matProp[2][dim]={0},		
-		
-		//Matriz Futura Total
-		matFutTot[dim][dim] = {
-			{80.0,70.0,100.0,90.0},
-			{120.0,90.0,70.0,60.0}
-		},
-		//Matriz Auxiliar Base
-		matAux[dim][dim]={0},
-
-		//Matriz Futura
-		matFut[dim][dim]{0};
-
-	//Declaraçao de variaveis uteis
-	float totViagensPresente=0, totViagensFuturas=0, prec=5;
-	int opcao=0;
-
-	
-	//						//
-	//	MATRIZ AUXILIAR		//
-	//						//
-
-	for(i=0;i<dim;i++){
-		for(j=0;j<dim;j++){
-			matAux[i][j]=matrizOriginal[i][j];
-		}
-	}
-	//						//
-	//	MATRIZ TOTAIS		//
-	//						//
-	for(i=0; i<dim;i++ ){
-		for(j=0; j<dim;j++){
-			aux[0]+=matAux[i][j]; //soma na linha
-			aux[1]+=matAux[j][i]; //soma na coluna
-		};
-		matTot[0][i]=aux[0];
-		matTot[1][i]=aux[1];
-
-		aux[0]=aux[1]=0;
-	};
-	//						//
-	//	MATRIZ PROPORCAO	//
-	//						//
-	for(i=0;i<2;i++){
-		for(j=0;j<dim;j++){
-				matProp[i][j] = abs(matFutTot[i][j]/matTot[i][j]);
-		};
-	};
-	//Total de Viagens
-	for(i=0;i<dim;i++){
-		totViagensPresente+=matTot[0][i];
-		totViagensFuturas+=matFutTot[0][i];
-	};
-
-	cout<<"DISPLAY MATRIZ ORIGINAL"<<endl;
-	MostMatrix(&matrizOriginal[0][0], dim, dim);
-
-	cout<<"DISPLAY MATRIZ AUXILIAR"<<endl;
-	MostMatrix(&matAux[0][0],dim,dim);
-
-	cout<<"DISPLAY MATRIZ TOTAIS"<<endl;
-	MostMat2D(&matTot[0][0], dim);
-
-	cout<<"DISPLAY MATRIZ TOTAIS FUTURAS"<<endl;
-	MostMat2D(&matFutTot[0][0],dim);
-
-	cout<<"DISPLAY MATRIZ PROPORCAO"<<endl;
-	MostMat2D(&matProp[0][0], dim);
-
-	cout<<"total de viagens presentes = "<<totViagensPresente<<endl;
-	cout<<"total de viagens futuras = "<<totViagensFuturas<<endl;				
-	cout<<"Percentual de acerto = "<<abs(matProp[0][0]-1)*100<<"%"<<endl;
-	nl
-	nl
-	nl
-	nl
-	nl
-	nl
-	nl
-	nl
+	int opcao=3;
 	TabOD matrizOD;
+	
 	InitTab(&matrizOD);
 	ShowTab(&matrizOD);
-	nl
-	nl
-	nl
-	nl
-	nl
-	nl
+	
 	nl
 	nl
 	switch(opcao){
 		case 0:
-			MetUni(&matrizOD.tabOD_I[0][0], &matrizOD.tabViagFut[0][0], &matrizOD.tabProp[0][0], &matrizOD.tabViagPre[0][0] ,matrizOD.totViagFut, matrizOD.totViagPre, matrizOD.dim);
+			MetUni(&matrizOD.tabOD_I[0][0], &matrizOD.tabViagFut[0][0], &matrizOD.tabProp[0][0], &matrizOD.tabViagPre[0][0], matrizOD.totViagFut, matrizOD.totViagPre, matrizOD.dim, &matrizOD.prec_O);
+			ShowTab(&matrizOD);
 			break;
 
 		case 1:
-			MetMedio(&matAux[0][0], &matFutTot[0][0], &matProp[0][0], dim, prec);
+			MetMedio(&matrizOD.tabOD_I[0][0], &matrizOD.tabViagFut[0][0], &matrizOD.tabProp[0][0], &matrizOD.tabViagPre[0][0], matrizOD.dim, matrizOD.prec_I, &matrizOD.prec_O);
+			ShowTab(&matrizOD);
 			break;
 
 		case 2:
-			MetDet(&matAux[0][0], &matFutTot[0][0], &matProp[0][0], totViagensFuturas, totViagensPresente, dim, prec);			
+			MetDet(&matrizOD.tabOD_I[0][0], &matrizOD.tabViagFut[0][0], &matrizOD.tabProp[0][0], &matrizOD.tabViagPre[0][0], matrizOD.totViagFut, matrizOD.totViagPre, matrizOD.dim, matrizOD.prec_I, &matrizOD.prec_O);
+			ShowTab(&matrizOD);			
 			break;
 
 		case 3:
-			MetFrat(&matAux[0][0], &matFutTot[0][0], &matProp[0][0], totViagensFuturas, totViagensPresente, dim, prec);
+			MetFrat(&matrizOD.tabOD_I[0][0], &matrizOD.tabViagFut[0][0], &matrizOD.tabProp[0][0], &matrizOD.tabViagPre[0][0], &matrizOD.tabFrat[0][0], &matrizOD.tabVPProp[0][0], matrizOD.totViagFut, matrizOD.totViagPre, matrizOD.dim, matrizOD.prec_I, &matrizOD.prec_O);
+			ShowTab(&matrizOD);
 			break;
 
 		default:
@@ -186,7 +94,7 @@ int main(){
 //	DEFINICAO DAS SUB-ROTINAS	//
 //	PRINCIPAIS					//
 
-void MetUni(float* matA, float* matTF, float* matP,float* matT ,float totViaFut, float totViaPres, int d){
+void MetUni(float* matA, float* matTF, float* matP, float* matT, float totViaFut, float totViaPres, int d, float* p_O){
 	cout<<"//\t\t\t\t//\n//\tINICIO\t\t\t//\n//\tMETODO UNIFORME\t\t//\n//\t\t\t\t//"<<endl<<endl;	
 	
 	for(i=0;i<d;i++){
@@ -194,15 +102,6 @@ void MetUni(float* matA, float* matTF, float* matP,float* matT ,float totViaFut,
 			(*(matA + i*d + j))*=(totViaFut/totViaPres);
 		};
 	};
-
-	cout<<"DISPLAY MATRIZ FUTURA"<<endl;
-	for(i=0; i<d;i++ ){
-		for(j=0; j<d;j++){
-			cout<<*(matA + i*d + j)<<"\t";
-		};
-		cout<<endl;
-	};
-	cout<<endl;
 
 	//matTot
 	for(i=0; i<d;i++ ){
@@ -215,11 +114,6 @@ void MetUni(float* matA, float* matTF, float* matP,float* matT ,float totViaFut,
 
 		aux[0]=aux[1]=0;
 	};
-	cout<<"DISPLAY MATRIZ TOTAIS"<<endl<<"SAIDAS \t"<<"ENTRADAS"<<endl;
-	for(j=0;j<d;j++){
-		cout<<*(matT+0*d+j)<<"\t"<<*(matT+1*d+j)<<endl;
-	};
-	cout<<endl;
 
 	//matProp
 	for(i=0;i<2;i++){
@@ -227,19 +121,11 @@ void MetUni(float* matA, float* matTF, float* matP,float* matT ,float totViaFut,
 				*(matP +i*d+j) = abs((*(matTF+i*d+j))/(*(matT+i*d+j)));
 		};
 	};
-	cout<<"DISPLAY MATRIZ PROPORCAO"<<endl<<"SAIDAS \t\t"<<"ENTRADAS"<<endl;
-	for(j=0;j<d;j++){
-		cout<<*(matP +0*d+j)<<"\t\t"<<*(matP +1*d+j)<<endl;
-	};
-	cout<<endl;
-	
-	cout<<"Percentual de acerto = "<<abs(*matP-1)*100<<"%"<<endl;
+	*p_O = CalcPreci(matP, d);
 }
 
-void MetMedio(float* matA, float* matTF, float* matP, int d, int p){
-	float matT[2][d];
+void MetMedio(float* matA, float* matTF, float* matP, float* matT, int d, int p_I, float* p_O){
 	do{
-
 		cout<<"//\t\t\t\t//\n//\tINICIO\t\t\t//\n//\tCRESCIMENTO MEDIO\t\t//\n//\t\t\t\t//"<<endl<<endl;
 		for(i=0;i<d;i++){
 			for(j=0;j<d;j++){
@@ -247,53 +133,29 @@ void MetMedio(float* matA, float* matTF, float* matP, int d, int p){
 			};
 		};
 
-		cout<<"DISPLAY MATRIZ Auxiliar"<<endl;
-		for(i=0; i<d;i++ ){
-			for(j=0; j<d;j++){
-
-				cout<<*(matA + i*d + j)<<"\t";
-			};
-			cout<<endl;
-		};
-		cout<<endl;
-
 		//matTot
 		for(i=0; i<d;i++ ){
 			for(j=0; j<d;j++){
 				aux[0]+=*(matA + i*d + j); //soma na linha
 				aux[1]+=*(matA + j*d + i); //soma na coluna
 			};
-			matT[0][i]=aux[0];
-			matT[1][i]=aux[1];
+			*(matT+0*d+i)=aux[0];
+			*(matT+1*d+i)=aux[1];
 
 			aux[0]=aux[1]=0;
 		};
-		cout<<"DISPLAY MATRIZ TOTAIS"<<endl<<"SAIDAS \t"<<"ENTRADAS"<<endl;
-		for(j=0;j<d;j++){
-			cout<<matT[0][j]<<"\t"<<matT[1][j]<<endl;
-		};
-		cout<<endl;
 
 		//matProp
 		for(i=0;i<2;i++){
 			for(j=0;j<d;j++){
-					*(matP+i*d+j) = abs(*(matTF+i*d+j)/matT[i][j]);
+					*(matP+i*d+j) = abs(*(matTF+i*d+j)/(*(matT+i*d+j)));
 			};
 		};
-		cout<<"DISPLAY MATRIZ PROPORCAO"<<endl<<"SAIDAS \t\t"<<"ENTRADAS"<<endl;
-		for(j=0;j<d;j++){
-			cout<<*(matP+0*d+j)<<"\t\t"<<*(matP+1*d+j)<<endl;
-		};
-		cout<<endl;
-		
-		cout<<"Percentual de acerto = "<<abs(*matP-1)*100<<"%"<<endl;
-
-	}while((abs(*matP-1)*100)>p);
+		*p_O = CalcPreci(matP, d);
+	}while(*p_O>p_I);
 }
 
-void MetDet(float* matA, float* matTF, float* matP, float totViaFut, float totViaPres, int d, int p){
-	float matT[2][d];
-
+void MetDet(float* matA, float* matTF, float* matP, float* matT, float totViaFut, float totViaPres, int d, int p_I, float* p_O){
 	do{
 		cout<<"//\t\t\t\t//\n//\tINICIO\t\t\t//\n//\tMETODO DETROIT\t\t//\n//\t\t\t\t//"<<endl<<endl;
 		for(i=0;i<d;i++){
@@ -308,102 +170,46 @@ void MetDet(float* matA, float* matTF, float* matP, float totViaFut, float totVi
 				aux[0]+=*(matA + i*d + j); //soma na linha
 				aux[1]+=*(matA + j*d + i); //soma na coluna
 			};
-			matT[0][i]=aux[0];
-			matT[1][i]=aux[1];
+			*(matT+0*d+i)=aux[0];
+			*(matT+1*d+i)=aux[1];
 
 			aux[0]=aux[1]=0;
 		};
 		//Total de Viagens				
 		for(i=0;i<d;i++){
-			totViaPres+=matT[0][i];
+			totViaPres+=(*(matT+0*d+i));
 		};
 		//matProp
 		for(i=0;i<2;i++){
 			for(j=0;j<d;j++){
-					*(matP+i*d+j) = abs(*(matTF+i*d+j)/matT[i][j]);
+					*(matP+i*d+j) = abs(*(matTF+i*d+j)/(*(matT+i*d+j)));
 			};
 		};
-
-		cout<<"DISPLAY MATRIZ AUXILIARr"<<endl;
-		for(i=0; i<d;i++ ){
-			for(j=0; j<d;j++){
-				cout<<*(matA + i*d + j)<<"\t";
-			};
-			cout<<endl;
-		};
-		cout<<endl;
-		cout<<"DISPLAY MATRIZ TOTAIS"<<endl;
-		for(i=0;i<2;i++){
-			if(i==0) cout<<"SAIDA\t:";
-			else cout<<"ENTRADA\t:";
-			for(j=0;j<d;j++){
-				cout<<matT[i][j]<<"\t";
-			};
-			cout<<endl;
-		}
-		cout<<endl;
-
-		cout<<"DISPLAY MATRIZ TOTAIS FUTURAS"<<endl;
-		for(i=0;i<2;i++){
-			if(i==0) cout<<"SAIDA\t:";
-			else cout<<"ENTRADA\t:";
-			for(j=0;j<d;j++){
-				cout<<*(matTF+i*d+j)<<"\t";
-			};
-			cout<<endl;
-		}
-		cout<<endl;
-
-		cout<<"DISPLAY MATRIZ PROPORCAO"<<endl;
-		for(i=0;i<2;i++){
-			if(i==0) cout<<"SAIDA\t:";
-			else cout<<"ENTRADA\t:";
-			for(j=0;j<d;j++){
-				cout<<*(matP+i*d+j)<<"\t";
-			};
-			cout<<endl;
-		}
-		cout<<endl;
-
-		cout<<"total de viagens presentes = "<<totViaPres<<endl;
-		cout<<"total de viagens futuras = "<<totViaFut<<endl;				
-		cout<<"Percentual de acerto = "<<abs(*matP-1)*100<<"%"<<endl;
-
-	}while((abs(*matP-1)*100)>p);
+		*p_O = CalcPreci(matP, d);
+	}while(*p_O>p_I);
 }
 
-void MetFrat(float* matA, float* matTF, float* matP, float totViaFut, float totViaPres, int d, int p){
+void MetFrat(float* matA, float* matTF, float* matP, float* matT, float* matFrat, float* matTotProp, float totViaFut, float totViaPres, int d, int p_I, float* p_O){
 	//Matriz Fratar
-	float matFrat[2][d]={0}, matTotProp[2][d]={0}, matT[2][d];
 
 	for(i=0; i<d;i++ ){
 		for(j=0; j<d;j++){
 			aux[0]+=(*(matA + i*d + j))*(*(matP+1*d+j)); //soma na linha
 			aux[1]+=(*(matA + j*d + i))*(*(matP+0*d+j)); //soma na coluna
 		};
-		matTotProp[0][i]=aux[0];
-		matTotProp[1][i]=aux[1];
+		*(matTotProp+0*d+i)=aux[0];
+		*(matTotProp+1*d+i)=aux[1];
 
 		aux[0]=aux[1]=0;
 	};
-	cout<<"DISPLAY MATRIZ TOTAIS PROPORCIONAIS"<<endl;
-	for(i=0;i<2;i++){
-		if(i==0) cout<<"SAIDA\t:";
-		else cout<<"ENTRADA\t:";
-		for(j=0;j<d;j++){
-			cout<<matTotProp[i][j]<<"\t";
-		};
-		cout<<endl;
-	}
-	cout<<endl;
 	//matTot
 		for(i=0; i<d;i++ ){
 			for(j=0; j<d;j++){
 				aux[0]+=*(matA + i*d + j); //soma na linha
 				aux[1]+=*(matA + j*d + i); //soma na coluna
 			};
-			matT[0][i]=aux[0];
-			matT[1][i]=aux[1];
+			*(matT+0*d+i)=aux[0];
+			*(matT+1*d+i)=aux[1];
 
 			aux[0]=aux[1]=0;
 		};
@@ -411,25 +217,15 @@ void MetFrat(float* matA, float* matTF, float* matP, float totViaFut, float totV
 
 	for(i=0;i<2;i++){
 		for(j=0;j<d;j++){
-			matFrat[i][j]=matT[i][j]/matTotProp[i][j];
+			*(matFrat+i*d+j)=(*(matT+i*d+j))/(*(matTotProp+i*d+j));
 		};
 	};
-	cout<<"DISPLAY MATRIZ FRATAR"<<endl;
-	for(i=0;i<2;i++){
-		if(i==0) cout<<"SAIDA\t:";
-		else cout<<"ENTRADA\t:";
-		for(j=0;j<d;j++){
-			cout<<matFrat[i][j]<<"\t";
-		};
-		cout<<endl;
-	}
-	cout<<endl;
 
 	do{
 		cout<<"//\t\t\t\t//\n//\tINICIO\t\t\t//\n//\tMETODO FRATAR\t\t//\n//\t\t\t\t//"<<endl<<endl;
 		for(i=0;i<d;i++){
 			for(j=0;j<d;j++){
-				*(matA + i*d + j)*=(*(matP+0*d+i))*(*(matP+1*d+j))*(matFrat[0][i]+matFrat[1][j])/2;
+				*(matA + i*d + j)*=(*(matP+0*d+i))*(*(matP+1*d+j))*(*(matFrat+0*d+i)+*(matFrat+1*d+j))/2;
 			};
 		};
 		totViaPres=0;
@@ -439,19 +235,19 @@ void MetFrat(float* matA, float* matTF, float* matP, float totViaFut, float totV
 				aux[0]+=*(matA + i*d + j); //soma na linha
 				aux[1]+=*(matA + j*d + i); //soma na coluna
 			};
-			matT[0][i]=aux[0];
-			matT[1][i]=aux[1];
+			*(matT+0*d+i)=aux[0];
+			*(matT+1*d+i)=aux[1];
 
 			aux[0]=aux[1]=0;
 		};
 		//Total de Viagens				
 		for(i=0;i<d;i++){
-			totViaPres+=matT[0][i];
+			totViaPres+=(*(matT+0*d+i));
 		};
 		//matProp
 		for(i=0;i<2;i++){
 			for(j=0;j<d;j++){
-					*(matP+i*d+j) = abs(*(matTF+i*d+j)/matT[i][j]);
+					*(matP+i*d+j) = abs(*(matTF+i*d+j)/(*(matT+i*d+j)));
 			};
 		};
 		//matTotProp				
@@ -460,8 +256,8 @@ void MetFrat(float* matA, float* matTF, float* matP, float totViaFut, float totV
 				aux[0]+=*(matA + i*d + j)*(*(matP+1*d+j)); //soma na linha
 				aux[1]+=(*(matA + j*d + i))*(*(matP+0*d+j)); //soma na coluna
 			};
-			matTotProp[0][i]=aux[0];
-			matTotProp[1][i]=aux[1];
+			*(matTotProp+0*d+i)=aux[0];
+			*(matTotProp+1*d+i)=aux[1];
 
 			aux[0]=aux[1]=0;
 		};
@@ -469,78 +265,11 @@ void MetFrat(float* matA, float* matTF, float* matP, float totViaFut, float totV
 		
 		for(i=0;i<2;i++){
 			for(j=0;j<d;j++){
-				matFrat[i][j]=matT[i][j]/matTotProp[i][j];
+				*(matFrat+i*d+j)=(*(matT+i*d+j))/(*(matTotProp+i*d+j));
 			};
 		};
-
-		cout<<"DISPLAY MATRIZ AUXILIARr"<<endl;
-		for(i=0; i<d;i++ ){
-			for(j=0; j<d;j++){
-				cout<<*(matA + i*d + j)<<"\t";
-			};
-			cout<<endl;
-		};
-		cout<<endl;
-		cout<<"DISPLAY MATRIZ TOTAIS"<<endl;
-		for(i=0;i<2;i++){
-			if(i==0) cout<<"SAIDA\t:";
-			else cout<<"ENTRADA\t:";
-			for(j=0;j<d;j++){
-				cout<<matT[i][j]<<"\t";
-			};
-			cout<<endl;
-		}
-		cout<<endl;
-
-		cout<<"DISPLAY MATRIZ TOTAIS FUTURAS"<<endl;
-		for(i=0;i<2;i++){
-			if(i==0) cout<<"SAIDA\t:";
-			else cout<<"ENTRADA\t:";
-			for(j=0;j<d;j++){
-				cout<<*(matTF+i*d+j)<<"\t";
-			};
-			cout<<endl;
-		}
-		cout<<endl;
-
-		cout<<"DISPLAY MATRIZ PROPORCAO"<<endl;
-		for(i=0;i<2;i++){
-			if(i==0) cout<<"SAIDA\t:";
-			else cout<<"ENTRADA\t:";
-			for(j=0;j<d;j++){
-				cout<<*(matP+i*d+j)<<"\t";
-			};
-			cout<<endl;
-		}
-		cout<<endl;
-
-		cout<<"DISPLAY MATRIZ TOTAIS PROPORCIONAIS"<<endl;
-		for(i=0;i<2;i++){
-			if(i==0) cout<<"SAIDA\t:";
-			else cout<<"ENTRADA\t:";
-			for(j=0;j<d;j++){
-				cout<<matTotProp[i][j]<<"\t";
-			};
-			cout<<endl;
-		}
-		cout<<endl;
-
-		cout<<"DISPLAY MATRIZ FRATAR"<<endl;
-		for(i=0;i<2;i++){
-			if(i==0) cout<<"SAIDA\t:";
-			else cout<<"ENTRADA\t:";
-			for(j=0;j<d;j++){
-				cout<<matFrat[i][j]<<"\t";
-			};
-			cout<<endl;
-		}
-		cout<<endl;
-
-		cout<<"total de viagens presentes = "<<totViaPres<<endl;
-		cout<<"total de viagens futuras = "<<totViaFut<<endl;				
-		cout<<"Percentual de acerto = "<<abs(*matP-1)*100<<"%"<<endl;
-
-	}while((abs(*matP-1)*100)>p);
+		*p_O = CalcPreci(matP, d);
+	}while(*p_O>p_I);
 }
 
 //								//
@@ -578,7 +307,8 @@ void InitTab(TabOD* tabela){
 		tabela->tabFrat[0][i]=(tabela->tabViagPre[0][i])/(tabela->tabVPProp[0][i]);
 		tabela->tabFrat[1][i]=(tabela->tabViagPre[1][i])/(tabela->tabVPProp[1][i]);
 		aux[0]=aux[1]=0;
-	};	
+	};
+	tabela->prec_O = CalcPreci(&tabela->tabProp[0][0], tabela->dim);
 }
 
 void ShowTab(TabOD* tabela){
@@ -603,6 +333,8 @@ void ShowTab(TabOD* tabela){
 	cout<<"Tabela Fratar";nl
 	MostMat2D(&tabela->tabFrat[0][0], tabela->dim);nl
 
+	cout<<"Precisão da Tabela:";nl
+	cout<<tabela->prec_O;nl
 }
 
 //								//
@@ -629,4 +361,17 @@ void MostMat2D(float* mat, int col){
 		cout<<endl;
 	}
 	cout<<endl;
+}
+
+float CalcPreci(float* mat, int d){
+	float p;
+	aux[0]=abs((*mat)-1);
+	for(i=0;i<2;i++){
+		for ( j = 0; j < d; j++){
+			if(aux[0]<abs(*(mat+i*d+j)-1)) aux[0]=abs(*(mat+i*d+j)-1);
+		};
+	};
+	p=100 * aux[0];
+	aux[0]=0;
+	return p;
 }
